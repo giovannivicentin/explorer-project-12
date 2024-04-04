1f8138a9ed7d12dec35bea35425c3885b29c463c
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-04 16:49:23 -0300

    feat: create /users endpoint and use query params for the request

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # explorer-project-12
- introduction a nodejs
+
+introduction a nodejs
+
+learn about http methods and route params and query params
--- src/server.js
+++ src/server.js
@@ -6,6 +6,17 @@ app.get('/message/:id/:user', (request, response) => {
   response.send(`Message ${id} from ${user}`);
 });
 
+// example how to use:
+// http://localhost:3000/message/1/John
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Users page ${page} with limit ${limit}`);
+});
+
+// example how to use:
+// http://localhost:3000/users?page=2&limit=3
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
