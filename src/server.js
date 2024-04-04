68417886c8dabfe12d293860a870f90fd02eb372
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-04 11:46:50 -0300

    feat: create and learn route params

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/message/:id', (request, response) => {
+  response.send('Hello World');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
